fb58159ac4b97d40143d07a6f612c9069bc0c4ae
Yasir <112541705+sirosiyasir@users.noreply.github.com>
2022-10-09 19:54:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
+
+var rates = document.querySelectorAll(".all-butn");
+var rating = document.getElementById("deneme");
+
+
+
 document.querySelector(".butnSubmit").addEventListener("click",function (){
   var text1 = document.querySelector(".firstText");
   var text2 = document.querySelector(".secondText");
@@ -10,51 +16,58 @@ document.querySelector(".butnSubmit").addEventListener("click",function (){
 
 })
 
+  // rates.forEach((rates)=>{
+  //   rates.addEventListener("click", function () {
+  //   rates.style.backgroundColor = "hsl(25, 97%, 53%)";
+  // })
+  // })
 
+  rates.forEach((rates) => {
+    rates.addEventListener("click", function() {
+      rates.style.backgroundColor = "hsl(25, 97%, 53%)";
+    })
+  })
 
-  document.querySelector(".butn1").addEventListener("click", butnFunction);
-  function butnFunction (){
-    document.querySelector(".butn1").style.backgroundColor ="hsl(25, 97%, 53%)";
-}
-
-
-
-
+  rates.forEach((rates) => {
+    rates.addEventListener("click", function() {
+      rating.innerHTML = rates.innerHTML
+    })
+  })
 // var i = 0;
 // while (i<5){
 // i++;
 // var deneme = ".butn" + i;
 // //First Button
-document.querySelector(".butn1").addEventListener("click", function (){
-
-document.querySelector(".yourSelectText").innerHTML = "you selected 1 out of 5";
-
-})
-
-//Second Button
-document.querySelector(".butn2").addEventListener("click", function (){
-
-document.querySelector(".yourSelectText").innerHTML = "you selected 2 out of 5";
-
-})
-
-//Third Button
-document.querySelector(".butn3").addEventListener("click", function (){
-
-document.querySelector(".yourSelectText").innerHTML = "you selected 3 out of 5";
-
-})
-
-//Fourth Button
-document.querySelector(".butn4").addEventListener("click", function (){
-
-document.querySelector(".yourSelectText").innerHTML = "you selected 4 out of 5";
-
-})
-
-//Fifth Button
-document.querySelector(".butn5").addEventListener("click", function (){
-
-document.querySelector(".yourSelectText").innerHTML = "you selected 5 out of 5";
-
-})
+// document.querySelector(".butn1").addEventListener("click", function (){
+//
+// document.querySelector(".yourSelectText").innerHTML = "you selected 1 out of 5";
+//
+// })
+//
+// //Second Button
+// document.querySelector(".butn2").addEventListener("click", function (){
+//
+// document.querySelector(".yourSelectText").innerHTML = "you selected 2 out of 5";
+//
+// })
+//
+// //Third Button
+// document.querySelector(".butn3").addEventListener("click", function (){
+//
+// document.querySelector(".yourSelectText").innerHTML = "you selected 3 out of 5";
+//
+// })
+//
+// //Fourth Button
+// document.querySelector(".butn4").addEventListener("click", function (){
+//
+// document.querySelector(".yourSelectText").innerHTML = "you selected 4 out of 5";
+//
+// })
+//
+// //Fifth Button
+// document.querySelector(".butn5").addEventListener("click", function (){
+//
+// document.querySelector(".yourSelectText").innerHTML = "you selected 5 out of 5";
+//
+// })
